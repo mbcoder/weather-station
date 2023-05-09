81920b2c3cf276c427386fd156934d75ddb752cc
mbcoder <mbaird@esri.com>
2023-05-09 13:44:50 +0100

    Initial experiments to show Raspberry Pi can write into feature layer

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,17 +17,35 @@
 package com.mycompany.app;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureTableEditResult;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.data.ServiceGeodatabase;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-    private MapView mapView;
+    //private MapView mapView;
+    private static final String SERVICE_LAYER_URL =
+        "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer";
+    private ServiceFeatureTable featureTable;
 
     public static void main(String[] args) {
 
@@ -48,22 +66,118 @@ public class App extends Application {
         Scene scene = new Scene(stackPane);
         stage.setScene(scene);
 
+        Button button = new Button("press me");
+        button.setOnAction(event -> {
+            System.out.println("pressed!");
+
+
+            Random random = new Random();
+
+            // create a map point from a point
+            Point point = new Point(random.nextDouble()* 10000, random.nextDouble() * 10000, SpatialReferences.getWebMercator());
+
+            // add a new feature to the service feature table
+            addFeature(point, featureTable);
+
+        });
+
+
+
+        // create a service geodatabase from the service layer url and load it
+        var serviceGeodatabase = new ServiceGeodatabase(SERVICE_LAYER_URL);
+        serviceGeodatabase.addDoneLoadingListener(() -> {
+
+            // create service feature table from the service geodatabase's table first layer
+            featureTable = serviceGeodatabase.getTable(0);
+
+            featureTable.loadAsync();
+            featureTable.addDoneLoadingListener(()-> {
+                System.out.println("table loaded");
+                stackPane.getChildren().add(button);
+            });
+
+            // create a feature layer from table
+            //var featureLayer = new FeatureLayer(featureTable);
+
+        });
+        serviceGeodatabase.loadAsync();
+
+        serviceGeodatabase.addDoneLoadingListener(()-> {
+            System.out.println("loaded db");
+        });
+
+
+
         // Note: it is not best practice to store API keys in source code.
         // An API key is required to enable access to services, web maps, and web scenes hosted in ArcGIS Online.
         // If you haven't already, go to your developer dashboard to get your API key.
         // Please refer to https://developers.arcgis.com/java/get-started/ for more information
-        String yourApiKey = "YOUR_API_KEY";
-        ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
+        //String yourApiKey = "YOUR_API_KEY";
+        //ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
 
         // create a MapView to display the map and add it to the stack pane
-        mapView = new MapView();
-        stackPane.getChildren().add(mapView);
+        //mapView = new MapView();
+        //stackPane.getChildren().add(mapView);
 
         // create an ArcGISMap with an imagery basemap
-        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
+        //ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
 
         // display the map by setting the map on the map view
-        mapView.setMap(map);
+        //mapView.setMap(map);
+    }
+
+
+    /**
+     * Adds a new Feature to a ServiceFeatureTable and applies the changes to the
+     * server.
+     *
+     * @param mapPoint location to add feature
+     * @param featureTable service feature table to add feature
+     */
+    private void addFeature(Point mapPoint, ServiceFeatureTable featureTable) {
+
+        // create default attributes for the feature
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("typdamage", "Destroyed");
+        attributes.put("primcause", "Earthquake");
+
+        // creates a new feature using default attributes and point
+        Feature feature = featureTable.createFeature(attributes, mapPoint);
+
+        // check if feature can be added to feature table
+        if (featureTable.canAdd()) {
+            // add the new feature to the feature table and to server
+            featureTable.addFeatureAsync(feature).addDoneListener(() -> applyEdits(featureTable));
+        } else {
+            System.out.println("Cannot add a feature to this feature table");
+        }
+    }
+
+    /**
+     * Sends any edits on the ServiceFeatureTable to the server.
+     *
+     * @param featureTable service feature table
+     */
+    private void applyEdits(ServiceFeatureTable featureTable) {
+
+        // apply the changes to the server
+        ListenableFuture<List<FeatureTableEditResult>> editResult = featureTable.getServiceGeodatabase().applyEditsAsync();
+        editResult.addDoneListener(() -> {
+            try {
+                List<FeatureTableEditResult> edits = editResult.get();
+                // check if the server edit was successful
+                if (edits != null && edits.size() > 0) {
+                    var featureEditResult = edits.get(0).getEditResult().get(0);
+                    if (!featureEditResult.hasCompletedWithErrors()) {
+                        System.out.println("Feature successfully added");
+                    } else {
+                        throw featureEditResult.getError();
+                    }
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("Exception applying edits on server" + e.getCause().getMessage());
+            }
+        });
     }
 
     /**
@@ -72,8 +186,11 @@ public class App extends Application {
     @Override
     public void stop() {
 
+        /*
         if (mapView != null) {
             mapView.dispose();
         }
+
+         */
     }
 }
