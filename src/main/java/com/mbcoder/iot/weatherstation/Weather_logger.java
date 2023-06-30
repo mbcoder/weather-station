7b2012cd66aa43690e892371616346460ce0cfcf
mbcoder <mbaird@esri.com>
2023-06-30 20:57:51 +0100

    Adding very basic IU

--- src/main/java/com/mbcoder/iot/weatherstation/Weather_logger.java
+++ src/main/java/com/mbcoder/iot/weatherstation/Weather_logger.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2023 Esri
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,13 +16,9 @@
 
 package com.mbcoder.iot.weatherstation;
 
-
-import com.esri.arcgisruntime.data.ServiceFeatureTable;
 import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
 import com.pi4j.devices.bmp280.BMP280Declares;
 import com.pi4j.devices.bmp280.BMP280Device;
-import com.pi4j.io.serial.Serial;
 import com.pi4j.plugin.linuxfs.provider.i2c.LinuxFsI2CProvider;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -30,7 +26,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -104,12 +99,5 @@ public class Weather_logger extends Application {
      */
     @Override
     public void stop() {
-
-        /*
-        if (mapView != null) {
-            mapView.dispose();
-        }
-
-         */
     }
 }
