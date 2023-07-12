c60bab5a7a2666312e58883923da9c2ef6bf9545
mbcoder <mbaird@esri.com>
2023-07-12 18:29:06 +0100

    Adding python sensor reader

---
+++ read-sensor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import time
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+from bme280 import BME280
+
+# Initialise the BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+bme280.setup(mode="forced")
+
+temperature = bme280.get_temperature()
+pressure = bme280.get_pressure()
+humidity = bme280.get_humidity()
+
+#print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
+print('Temp:{:05.2f}'.format(temperature))
+print('Pressure:{:05.2f}'.format(pressure))
+print('Humidity:{:05.2f}'.format(humidity))
