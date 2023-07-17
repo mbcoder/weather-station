c1aff089a3a46e74c3a56559b543e56ee4999fa7
mbcoder <mbaird@esri.com>
2023-07-17 11:45:39 +0100

    fixing python reader

---
+++ test.py
@@ -0,0 +1,17 @@
+#while :
+#do
+#  echo Temp:21.14
+#  echo Pressure:682.86
+#  echo Humidity:63.41
+#  sleep 2
+#done
+
+
+import time
+
+while True:
+    print('Temp:21.14')
+    print('Pressure:682.86')
+    print('Humidity:63.41')
+    time.sleep(2)
+
