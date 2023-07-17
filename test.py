f6e334ee12ff302e3ddef79eed6da4f28649f4e0
mbcoder <mbaird@esri.com>
2023-07-17 12:46:26 +0100

    fixing python reader

--- test.py
+++ test.py
@@ -11,9 +11,9 @@ import time
 import random
 
 while True:
+    humidity = random.randint(0,9)
     print('Temp:21.14')
     print('Pressure:682.86')
-    print('Humidity:63.41')
-    print(random.randint(0,9))
+    print('Humidity:{:05.2f}'.format(humidity))
     time.sleep(2)
 
