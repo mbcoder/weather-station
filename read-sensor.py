58ee0dc48e1aba48ee7466a7f97594aea73f32f5
mbcoder <mbaird@esri.com>
2023-07-17 13:00:02 +0100

    fixing python reader

--- read-sensor.py
+++ read-sensor.py
@@ -7,14 +7,14 @@ except ImportError:
     from smbus import SMBus
 from bme280 import BME280
 
-# Initialise the BME280
-bus = SMBus(1)
-bme280 = BME280(i2c_dev=bus)
-
 print('alive!!!')
 print('Temp:21.14')
 print('Pressure:682.86')
 print('Humidity:22.0')
+
+# Initialise the BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
 
 while True:
     temperature = bme280.get_temperature()
